387bf01489069341c67e6685d2646f166cf1f4a1
DWH-100524 <danielhilfeldt@gmail.com>
2025-06-24 16:17:35 +0100

    1D Lattice - Hamilonian Analysis.py

--- 1D Lattice - Hamilonian Analysis.py
+++ 1D Lattice - Hamilonian Analysis.py
@@ -11,13 +11,15 @@ import matplotlib.pyplot as plt
 rng=np.random.default_rng()
 fig, R=plt.subplots()
 
-a=1                     # - Constant
+a=1                     # - Hopping Constant
 n=200                   # - #Sites
 m=1.0                   # - Disorder Coefficient
 
+# Creating the Hamiltonian matrix, and a list N for plotting purposes
 N=list(range(n))
 H=np.random.rand(n,n)
 
+# Replacing each value of the Hamiltonian with the given theoretical value
 for i in range(n):
     for j in range (n):
         if j==i:
@@ -32,9 +34,11 @@ H[0][n-1]=-a
 H[n-1][0]=-a
 H=H.astype(complex)
 
+# Finding the eigenvalues of the system, and ordering them in an increasing list
 E,v=eig(H)
 E.sort()
 
+# Plotting the energy levels of the system
 R.plot(N,E)
 plt.show()
 
